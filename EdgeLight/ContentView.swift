1473ed0de9d3557c5d85e675e330c18adf0074d8
nrksu1tan <nurekesh00@mail.ru>
2025-12-13 02:51:56 +0500

    Initial Commit

---
+++ EdgeLight/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  EdgeLight
+//
+//  Created by Nursultan Nurekesh on 13.12.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ EdgeLight/EdgeLightApp.swift
@@ -0,0 +1,17 @@
+//
+//  EdgeLightApp.swift
+//  EdgeLight
+//
+//  Created by Nursultan Nurekesh on 13.12.2025.
+//
+
+import SwiftUI
+
+@main
+struct EdgeLightApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
